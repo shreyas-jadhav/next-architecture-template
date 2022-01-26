66a7329db0559ec08e5444d51124287c7ca6d222
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-26 17:50:05 +0530

    with auth middleware

---
+++ server/middelwares/withAuth.ts
@@ -0,0 +1,11 @@
+import { getSession } from "next-auth/react"
+import { UnauthorizedError } from "server/errors";
+import { ApiHandler } from "server/types"
+
+export const withAuth = (handler: ApiHandler): ApiHandler => async (req, res) => {
+    const session = await getSession({ req });
+    
+    if (!session) throw new UnauthorizedError();
+    return await handler(req,res)
+}
+
